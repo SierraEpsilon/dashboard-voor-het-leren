5632dcdca3f17a4907832c71bdfd2c73ccf5aacc
SierraEpsilon <seb@canti.be>
2012-11-05 18:02:07 +0100

    small change

--- Dashboard/src/dashboard/servlet/LoginServlet.java
+++ Dashboard/src/dashboard/servlet/LoginServlet.java
@@ -5,10 +5,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import dashboard.error.InvalidLogInDataException;
 import dashboard.registry.UserRegistry;
 
 public class LoginServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -2516739187837047286L;
+
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		String username = req.getParameter("username");
 		String password = req.getParameter("password");
