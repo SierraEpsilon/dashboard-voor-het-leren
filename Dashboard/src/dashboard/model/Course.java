a43637d83f735bd7046328dd4d7c0a1579580add
SierraEpsilon <seb@canti.be>
2012-11-12 17:06:16 +0100

    course aangepast

--- Dashboard/src/dashboard/model/Course.java
+++ Dashboard/src/dashboard/model/Course.java
@@ -3,38 +3,24 @@ package dashboard.model;
 public class Course {
 
 	private final String name;
-	private final String[] diploma;
-	private final int phase;
 	private final int credit;
 	
 	/**
 	 * initiates a course
 	 * @param 	name
 	 * 	the name of the course
-	 * @param 	phase
-	 * 	the phase of the course
 	 * @param 	credit
 	 * 	the credit of the course
-	 * @param 	diploma
-	 * 	the diploma of the course
 	 * @post	
 	 * 	the name equals the given name
 	 * 	|	new.getName() = name
 	 * @post	
-	 * 	the phase equals the given phase
-	 * 	|	new.getPhase() = phase
-	 * @post	
 	 * 	the credit equals the given credit
 	 * 	|	new.getCredit() = credit
-	 * @post	
-	 * 	the diploma equals the given diploma
-	 * 	|	new.getDiploma() = diploma
 	 */
 	public Course(String name, int phase, int credit,String[] diploma){
 		this.name = name;
-		this.phase = phase;
 		this.credit = credit;
-		this.diploma = diploma;
 	}
 	
 	/**
@@ -57,43 +43,10 @@ public class Course {
 	
 	/**
 	 * @return
-	 * 	returns the phase of the course
-	 * 	|	phase
-	 */
-	public int getPhase() {
-		return phase;
-	}
-	
-	/**
-	 * @return
-	 * 	returns the possible diploma's 
-	 * 	| diploma
-	 */
-	public String[] getDiploma() {
-		return diploma;
-	}
-	
-	/**
-	 * @return
 	 * 	the hours you should study for that particular course
 	 * 	|	getCredit()*28
 	 */
 	public int getHoursNeeded(){
 		return getCredit()*28;
-	}
-	
-	/**
-	 * @param diploma
-	 * 	the diploma that has to be checked
-	 * @return
-	 * 	true if it is a possible diploma
-	 * 	|	if(diploma.contains(diploma))
-	 * 	|		return true
-	 */
-	public boolean isMatchingDiploma(String diploma){
-		for(String thisDiploma: getDiploma())
-			if(thisDiploma.equals(diploma))
-				return true;
-		return false;
 	}
 }
