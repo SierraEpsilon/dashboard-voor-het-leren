d90232022b25123c7a086b8f91f741b5a70d101a
Yannick Boesmans <yannick.boesmans@gmail.com>
2012-11-15 19:52:59 +0100

    course tracking

--- Dashboard/src/dashboard/registry/CourseRegistry.java
+++ Dashboard/src/dashboard/registry/CourseRegistry.java
@@ -10,11 +10,14 @@ import dashboard.model.Student;
 public class CourseRegistry {
 
 	private static HashMap<String,Branch> branches;
+	private static ArrayList<Course> courses;
 	
 	
 	static{
 		branches = new HashMap<String,Branch>();
+		courses = new ArrayList<Course>();
 		addBranches();
+		addCourses();
 	}
 
 	/**
@@ -22,6 +25,10 @@ public class CourseRegistry {
 	 */
 	private static void addBranches() {
 		branches.put(Branch.BABI1.getName(),Branch.BABI1);
+	}
+	
+	private static void addCourses(){
+		courses.addAll(Branch.BABI1.getCourses());
 	}
 	
 	/**
@@ -40,5 +47,14 @@ public class CourseRegistry {
 	 */
 	public static ArrayList<Course> getBranch(String branch){
 		return getBranches().get(branch).getCourses();
+	}
+	
+	public static Course getCourse(String coursename){
+		for(Course course: courses){
+			if(course.getName().equals(coursename)){
+				return course;
+			}
+		}
+		return null;
 	}
 }
