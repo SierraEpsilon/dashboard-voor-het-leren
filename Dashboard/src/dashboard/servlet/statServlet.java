a69042cb160698cbf683901ade1d77380138aa2e
SierraEpsilon <seb@canti.be>
2012-11-18 17:04:09 +0100

    stats meer naar debugger toe gewerkt, severe Exception op server moet nu berhinderd worden

--- Dashboard/src/dashboard/servlet/statServlet.java
+++ Dashboard/src/dashboard/servlet/statServlet.java
@@ -1,9 +1,21 @@
 package dashboard.servlet;
 
-import javax.servlet.http.HttpServlet;;
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class statServlet extends HttpServlet {
 
 	private static final long serialVersionUID = -6566808443793850095L;
 
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		HttpSession session = req.getSession();
+		if(session.getAttribute("student") != null)
+			resp.sendRedirect("/stats.jsp");
+		else
+			resp.sendRedirect("/error.jsp?msg=not logged in");
+	}
 }
