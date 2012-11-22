87bb4192306904c07c7e4adbc082539a666efcd4
Brun <Brun@10.43.14.10>
2012-11-22 16:36:51 +0100

    manual track servlet

---
+++ Dashboard/src/dashboard/servlet/ManualTrackingServlet.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dashboard.model.Student;
+import dashboard.registry.CourseRegistry;
+import dashboard.model.StudyMoment;
+import dashboard.model.Course;
+
+
+public class ManualTrackingServlet extends HttpServlet{
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		HttpSession session = req.getSession();
+		Student student = (Student)session.getAttribute("student");
+		
+		if(student!=null){
+			Date startDate = new Date(session.getAttribute("startdate") +" " + session.getAttribute("starttime"));
+			Date endDate = new Date(session.getAttribute("enddate") + " "+ session.getAttribute("endtime"));
+			
+			int amount = session.getAttribute("amount");
+			
+			Course usedCourse = CourseRegistry.getCourse((String) session.getAttribute("course"));
+			
+			String kind = session.getAttribute("kind");
+			
+			student.addStudyMoment(new StudyMoment(startDate, endDate, usedCourse, amount, kind));
+			
+		} else {
+			resp.sendRedirect("/login.jsp");
+		}
+	}
+	
+}
