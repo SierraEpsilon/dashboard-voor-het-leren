8291dba8f9b1f9c48c023ffc688ba01e440cfc87
sanderclaeys <sanderclaeys@gmail.com>
2012-11-28 15:14:59 +0100

    stat 1

--- Dashboard/src/dashboard/servlet/statServlet.java
+++ Dashboard/src/dashboard/servlet/statServlet.java
@@ -1,11 +1,20 @@
 package dashboard.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import com.google.appengine.labs.repackaged.org.json.JSONException;
+import com.google.appengine.labs.repackaged.org.json.JSONObject;
+import com.google.appengine.labs.repackaged.org.json.JSONArray;
+
+import dashboard.model.Course;
+import dashboard.model.Student;
+import dashboard.registry.StudentRegistry;
 
 public class statServlet extends HttpServlet {
 
@@ -13,9 +22,21 @@ public class statServlet extends HttpServlet {
 
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		HttpSession session = req.getSession();
+		PrintWriter writer = resp.getWriter();
 		if(session.getAttribute("student") != null)
-			resp.sendRedirect("/stats.jsp");
-		else
-			resp.sendRedirect("/error.jsp?msg=not logged in");
+	        writer.println("UNAUTHORIZED");
+		else{
+			JSONArray root = new JSONArray();
+			try {
+				JSONObject obj = new JSONObject();
+				obj.put("name", "Studiepunten");
+				root.put(obj);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			writer.println(root.toString());
+		}
+			
 	}
 }
