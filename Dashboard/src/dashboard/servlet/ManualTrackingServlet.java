95fb19ac2d897f9bdc23a9cc22e92f74d34ba76e
MartinCamerman <mcamerman@hotmail.com>
2012-11-29 17:45:35 +0100

    qsdf

--- Dashboard/src/dashboard/servlet/ManualTrackingServlet.java
+++ Dashboard/src/dashboard/servlet/ManualTrackingServlet.java
@@ -34,38 +34,50 @@ public class ManualTrackingServlet extends HttpServlet{
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		HttpSession session = req.getSession();
 		Student student = (Student)session.getAttribute("student");
-		SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy hh:mm");
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		
 			Date startD = new Date();
 			try {
-				startD = df.parse((String) req.getParameter("startdate") +" " + (String)req.getParameter("starttime"));
+				startD = df.parse((String) req.getParameter("startdate") +" " + (String)req.getParameter("starttime") + ":00" );
 			} catch (ParseException e2) {
 				// TODO Auto-generated catch block
 				e2.printStackTrace();
 			}
 			Date endD = new Date();
 			try {
-				endD = df.parse((String) req.getParameter("enddate") +" " + (String) req.getParameter("endtime"));
+				endD = df.parse((String) req.getParameter("enddate") +" " + (String) req.getParameter("endtime") + ":00" );
 			} catch (ParseException e2) {
 				// TODO Auto-generated catch block
 				e2.printStackTrace();
 			}
+			Date testDate = new Date();
 			
-			int number = (Integer) Integer.parseInt(req.getParameter("amount"));	
-			try {
-				Course usedCourse = CourseRegistry.getCourse((String) req.getParameter("courseinput"));
-				String kindStudied = (String) req.getParameter("kind");
-				student.addStudyMoment(new StudyMoment(startD, endD, usedCourse, number, kindStudied));
+			if(!endD.before(testDate)){
 				session.setAttribute("student", student);
-				resp.sendRedirect("/add_manual.jsp?msg=Studiemoment correct toegevoegd");
-			} catch (InvalidEndDateException e) {
-				resp.sendRedirect("/add_manual.jsp?msg=You appear to be a time traveler?!");
-			} catch (InvalidAmountException e) {
-				resp.sendRedirect("/add_manual.jsp?msg=You can't have studied that kind of pages!");
-			} catch (InvalidStudyMomentException e) {
-				resp.sendRedirect("/add_manual.jsp?msg=You were already studying at the time!");
-			} catch (NoSuchCourseException e1) {
-				resp.sendRedirect("/add_manual.jsp?msg=no such course!");
+				resp.sendRedirect("/add_manual.jsp?msg=Je kan geen studiemoment in de toekomst toevoegen!");
+			}
+			else{
+			
+				int number = (Integer) Integer.parseInt(req.getParameter("amount"));	
+				try {
+					Course usedCourse = CourseRegistry.getCourse((String) req.getParameter("courseinput"));
+					String kindStudied = (String) req.getParameter("kind");
+					student.addStudyMoment(new StudyMoment(startD, endD, usedCourse, number, kindStudied));
+					session.setAttribute("student", student);
+					resp.sendRedirect("/add_manual.jsp?msg=Studiemoment correct toegevoegd");
+				} catch (InvalidEndDateException e) {
+					session.setAttribute("student", student);
+					resp.sendRedirect("/add_manual.jsp?msg=Ongeldige data. Controleer of de einddatum na de begindatum is.");
+				} catch (InvalidAmountException e) {
+					session.setAttribute("student", student);
+					resp.sendRedirect("/add_manual.jsp?msg=You can't have studied that kind of pages!");
+				} catch (InvalidStudyMomentException e) {
+					session.setAttribute("student", student);
+					resp.sendRedirect("/add_manual.jsp?msg=Je was op dit moment reeds aan het studeren!");
+				} catch (NoSuchCourseException e1) {
+					session.setAttribute("student", student);
+					resp.sendRedirect("/add_manual.jsp?msg=no such course!");
+				}
 			}
 			
 	}
