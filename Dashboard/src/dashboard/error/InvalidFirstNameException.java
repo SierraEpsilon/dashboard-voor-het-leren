680238a630ed182799ccd937aaa37bc9d2bb9cfd
MartinCamerman <mcamerman@hotmail.com>
2012-12-12 09:54:28 +0100

    shsdgh

---
+++ Dashboard/src/dashboard/error/InvalidFirstNameException.java
@@ -0,0 +1,7 @@
+package dashboard.error;
+
+public class InvalidFirstNameException extends Exception {
+
+	private static final long serialVersionUID = -2755447826612363395L;
+
+}
---
+++ Dashboard/src/dashboard/error/InvalidLastNameException.java
@@ -0,0 +1,7 @@
+package dashboard.error;
+
+public class InvalidLastNameException extends Exception {
+
+	private static final long serialVersionUID = 139946912394918237L;
+
+}
